b8f21cb72a042f5a271ec6b9da7467733c6f31a0
Raphaël Baticle <61377616+Lasauce6@users.noreply.github.com>
2023-12-06 14:00:22 +0100

    tests

--- shell.c
+++ shell.c
@@ -5,22 +5,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+// int main() {
+// 	while(1) {
+// 		printf("$ ");
+// 		char *buf = malloc(1024);
+// 		scanf("%s", buf);
+//
+// 		if (strcmp(buf, "exit") == 0 || feof(stdin)) {
+// 			exit(0);
+// 		}
+//
+// 		if (fork() == 0) {	
+// 			execlp(buf, buf, NULL);
+// 		} else {
+// 			wait(NULL);
+// 		}
+//
+// 		free(buf);
+// 	}
+// }
+//
+
 int main() {
-	while(1) {
-		printf("$ ");
-		char *buf = malloc(1024);
-		scanf("%s", buf);
+	printf("$ ");
+	char *buf = malloc(1024);
+	scanf("%s", buf);
+	char *argv[1024];
+	int argc = parse_line(buf, argv);
 
-		if (strcmp(buf, "exit") == 0 || feof(stdin)) {
-			exit(0);
-		}
+	free(buf);
 
-		if (fork() == 0) {	
-			execlp(buf, buf, NULL);
-		} else {
-			wait(NULL);
-		}
 
-		free(buf);
+}
+
+int parse_line(char *s, char **argv []) {
+	// use strpbrk to find the first occurence of a character in a string
+	int i = 0;
+	char *p = strpbrk(s, " ");
+	while (p != NULL) {
+		argv[i] = s;
+		i++;
+		s = p + 1;
+		p = strpbrk(s, " ");
 	}
+	argv[i] = NULL;
+	return i;
 }
