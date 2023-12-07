211882c033ebd0f43fed6d023d10e6cdb6758d82
Raphaël <lasaucisse006@gmail.com>
2023-12-07 16:09:23 +0100

    parse_line

--- shell.c
+++ shell.c
@@ -1,53 +1,58 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-// int main() {
-// 	while(1) {
-// 		printf("$ ");
-// 		char *buf = malloc(1024);
-// 		scanf("%s", buf);
-//
-// 		if (strcmp(buf, "exit") == 0 || feof(stdin)) {
-// 			exit(0);
-// 		}
-//
-// 		if (fork() == 0) {	
-// 			execlp(buf, buf, NULL);
-// 		} else {
-// 			wait(NULL);
-// 		}
-//
-// 		free(buf);
-// 	}
-// }
-//
+int parse_line(char *s, char ***argv) {
+    *argv = (char **)malloc(sizeof(char *));
+    int count = 0;
 
-int main() {
-	printf("$ ");
-	char *buf = malloc(1024);
-	scanf("%s", buf);
-	char *argv[1024];
-	int argc = parse_line(buf, argv);
+    char *token = strtok(s, " \t\n");
+    while (token != NULL) {
+        (*argv)[count] = (char *)malloc(strlen(token) + 1);
+        strcpy((*argv)[count], token);
+        count++;
 
-	free(buf);
+        *argv = (char **)realloc(*argv, (count + 1) * sizeof(char *));
 
+        token = strtok(NULL, " \t\n");
+    }
 
+    (*argv)[count] = NULL;
+
+    return count;
 }
 
-int parse_line(char *s, char **argv []) {
-	// use strpbrk to find the first occurence of a character in a string
-	int i = 0;
-	char *p = strpbrk(s, " ");
-	while (p != NULL) {
-		argv[i] = s;
-		i++;
-		s = p + 1;
-		p = strpbrk(s, " ");
+int main() {
+	while (1) {
+		printf("$ ");
+		char *buf = malloc(1024);
+		scanf(" %[^\n]", buf);
+
+		if (strcmp(buf, "exit") == 0 || feof(stdin)) {
+			exit(0);
+		}
+
+		char **argv;
+		int argc = parse_line(buf, &argv);
+
+		if (fork() == 0) {
+			// enfant
+			execvp(argv[0], argv);
+			perror("Erreur dans execvp");
+			exit(EXIT_FAILURE);
+		} else {
+			// parent
+			wait(NULL);
+		}
+
+		for (int i = 0; i < argc; i++) {
+			free(argv[i]);
+		}
+		free(argv);
+		free(buf);
 	}
-	argv[i] = NULL;
-	return i;
 }
