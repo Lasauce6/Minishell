f7288df99651ed36e5b1effb5d618d793edff982
Raphaël <lasaucisse006@gmail.com>
2023-12-18 15:50:24 +0100

    tubes simples

--- shell.c
+++ shell.c
@@ -7,23 +7,23 @@
 #include <fcntl.h>
 
 int parse_line(char *s, char ***argv) {
-    *argv = (char **)malloc(sizeof(char *));
-    int count = 0;
+	*argv = (char **)malloc(sizeof(char *));
+	int count = 0;
 
-    char *token = strtok(s, " \t\n");
-    while (token != NULL) {
-        (*argv)[count] = (char *)malloc(strlen(token) + 1);
-        strcpy((*argv)[count], token);
-        count++;
+	char *token = strtok(s, " \t\n");
+	while (token != NULL) {
+		(*argv)[count] = (char *)malloc(strlen(token) + 1);
+		strcpy((*argv)[count], token);
+		count++;
 
-        *argv = (char **)realloc(*argv, (count + 1) * sizeof(char *));
+		*argv = (char **)realloc(*argv, (count + 1) * sizeof(char *));
 
-        token = strtok(NULL, " \t\n");
-    }
+		token = strtok(NULL, " \t\n");
+	}
 
-    (*argv)[count] = NULL;
+	(*argv)[count] = NULL;
 
-    return count;
+	return count;
 }
 
 int main() {
@@ -42,49 +42,98 @@ int main() {
 		char *output_file = NULL;
 		int saved_stdout = dup(STDOUT_FILENO);
 
-		for (int i = 0; i < argc; i++) {
-			if (strcmp(argv[i], ">") == 0) {
-				if (i + 1 < argc && argv[i + 1] != NULL && argv[i + 2] == NULL) {
-					output_file = strdup(argv[i + 1]);
-				}
-				break;
-
+		if (argc > 2) {
+			if (strcmp(argv[argc - 2], ">") == 0) {
+				output_file = strdup(argv[argc - 1]);
 			}
 		}
 
-		if (output_file != NULL) {
-			int fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			if (fd == -1) {
-				perror("Erreur lors de l'ouverture du fichier de sortie");
+		if (argc > 1 && strcmp(argv[argc - 1], "|") == 0) {
+			free(argv[argc - 1]);
+			argv[argc - 1] = NULL;
+			int pipefd[2];
+			if (pipe(pipefd) == -1) {
+				perror("Erreur lors de la création du tube");
 				exit(EXIT_FAILURE);
 			}
-			dup2(fd, STDOUT_FILENO);
-			argv[argc - 2] = NULL;
-			close(fd);
-		}
 
-		if (fork() == 0) {
-			// enfant
-			execvp(argv[0], argv);
-			perror("Erreur dans execvp");
-			exit(EXIT_FAILURE);
+			if (fork() == 0) {
+				// enfant (première commande)
+				close(pipefd[0]);
+				dup2(pipefd[1], STDOUT_FILENO);
+				close(pipefd[1]);
+
+				execvp(argv[0], argv);
+				perror("Erreur dans execvp");
+				exit(EXIT_FAILURE);
+			} else {
+				// parent
+				wait(NULL);
+
+				close(pipefd[1]);
+
+				char *buf2 = malloc(1024);
+				scanf(" %[^\n]", buf2);
+
+				char **argv2;
+				int argc2 = parse_line(buf2, &argv2);
+				free(buf2);
+
+				if (fork() == 0) {
+					// enfant (deuxième commande)
+					close(pipefd[1]);
+
+					dup2(pipefd[0], STDIN_FILENO);
+					close(pipefd[0]);
+
+					execvp(argv2[0], argv2);
+					perror("Erreur dans execvp");
+					exit(EXIT_FAILURE);
+				} else {
+					// parent
+					wait(NULL);
+
+					close(pipefd[0]);
+				}
+
+				for (int i = 0; i < argc2; i++) {
+					free(argv2[i]);
+				}
+				free(argv2);
+			}
 		} else {
-			// parent
-			wait(NULL);
+			if (output_file != NULL) {
+				int fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+				if (fd == -1) {
+					perror("Erreur lors de l'ouverture du fichier de sortie");
+					exit(EXIT_FAILURE);
+				}
+				dup2(fd, STDOUT_FILENO);
+				argv[argc - 2] = NULL;
+				close(fd);
+			}
+
+			if (fork() == 0) {
+				// enfant
+				execvp(argv[0], argv);
+				perror("Erreur dans execvp");
+				exit(EXIT_FAILURE);
+			} else {
+				// parent
+				wait(NULL);
+			}
+
+			if (output_file != NULL) {
+				dup2(saved_stdout, STDOUT_FILENO);
+				free(output_file);
+			}
+
+			for (int i = 0; i < argc; i++) {
+				free(argv[i]);
+			}
+			free(argv);
 		}
 
-		if (output_file != NULL) {
-			dup2(saved_stdout, STDOUT_FILENO);
-		}
-
-		for (int i = 0; i < argc; i++) {
-			free(argv[i]);
-		}
-		free(argv);
 		free(buf);
-		if (output_file != NULL) {
-			free(output_file);
-		}
-		
 	}
 }
